80295ee18468e32e758772097d755352208d1652
membraned <aidanheck@gmail.com>
2020-12-15 12:32:28 -0500

    endpoint troubleshooting

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ app.post(
     Users.find({ Username: req.body.Username })
       .then((user) => {
         if (user) {
-          return res.status(400).send(req.body.Username + ' already exists');
+          return res.status(400).json(user);
         } else {
           Users.create({
             Username: req.body.Username,
